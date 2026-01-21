9d8cfc4e2e6cd97de221d5db49dcb197b04bb3b5
Hernan Coronel <dam.hernan.coronel@gmail.com>
2026-01-21 20:39:06 +0100

    initial commit

---
+++ src/main/java/es/coronelhernan/kitchapp/backend/KitchApp/KitchAppApplication.java
@@ -0,0 +1,13 @@
+package es.coronelhernan.kitchapp.backend.KitchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/coronelhernan/kitchapp/backend/KitchApp/KitchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package es.coronelhernan.kitchapp.backend.KitchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
