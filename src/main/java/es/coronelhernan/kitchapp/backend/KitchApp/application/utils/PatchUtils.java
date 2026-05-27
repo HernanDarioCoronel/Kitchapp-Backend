00aba788ab9277f6310bb6210c02ae4b07353000
Hernán Darío Coronel <dam.hernan.coronel@gmail.com>
2026-05-27 19:04:56 +0200

    fix patchutils

--- src/main/java/es/coronelhernan/kitchapp/backend/KitchApp/application/utils/PatchUtils.java
+++ src/main/java/es/coronelhernan/kitchapp/backend/KitchApp/application/utils/PatchUtils.java
@@ -1,9 +1,8 @@
 package es.coronelhernan.kitchapp.backend.KitchApp.application.utils;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
-
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,27 +11,25 @@ public final class PatchUtils {
     private PatchUtils() {}
 
     public static void copyNonNullProperties(Object src, Object target, String... alwaysIgnore) {
-        BeanWrapper srcWrap = new BeanWrapperImpl(src);
-        java.beans.PropertyDescriptor[] pds = srcWrap.getPropertyDescriptors();
+        Set<String> ignoreSet = alwaysIgnore != null ? new HashSet<>(Arrays.asList(alwaysIgnore)) : new HashSet<>();
 
-        Set<String> emptyNames = new HashSet<>();
-        for (java.beans.PropertyDescriptor pd : pds) {
-            String name = pd.getName();
-            if (srcWrap.isReadableProperty(name)) {
-                Object srcValue = srcWrap.getPropertyValue(name);
-                if (srcValue == null) {
-                    emptyNames.add(name);
+        Class<?> clazz = src.getClass();
+        while (clazz != null && clazz != Object.class) {
+            for (Field field : clazz.getDeclaredFields()) {
+                if (ignoreSet.contains(field.getName())) continue;
+                if (Modifier.isStatic(field.getModifiers())) continue;
+
+                field.setAccessible(true);
+                try {
+                    Object value = field.get(src);
+                    if (value != null) {
+                        field.set(target, value);
+                    }
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("No se pudo copiar el campo: " + field.getName(), e);
                 }
             }
+            clazz = clazz.getSuperclass();
         }
-
-        if (alwaysIgnore != null) {
-            for (String n : alwaysIgnore) {
-                emptyNames.add(n);
-            }
-        }
-
-        String[] ignoreArray = new String[emptyNames.size()];
-        BeanUtils.copyProperties(src, target, emptyNames.toArray(ignoreArray));
     }
 }
